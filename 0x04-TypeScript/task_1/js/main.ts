101320dbddf9d70b86cf673ac5291aae64b3f21f
Precious Ogwezhi <pogwezhi@gmail.com>
2025-05-23 16:35:15 +0100

    task

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,5 +15,40 @@ interface Teacher {
     location: "Lagos",
     contract: false, // additional property allowed
   };
-  
   console.log(teacher1);
+
+  interface Directors extends Teacher {
+    numberOfReports: number;
+  }
+  
+  function printTeacher (firstName: string, lastName: string): string {
+    return `${firstName.charAt(0)}. ${lastName}`
+  }
+ 
+  interface StudentConstructor {
+    firstName: string,
+    lastName: string
+  }
+
+  interface StudentInterface {
+    workOnHomework(): string,
+    displayName(): string
+  }
+
+  class StudentClass implements StudentInterface {
+    private firstName: string;
+    private lastName: string;
+  
+    constructor({ firstName, lastName }: StudentConstructor) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  
+    workOnHomework(): string {
+      return "Currently working";
+    }
+  
+    displayName(): string {
+      return this.firstName;
+    }
+  }
